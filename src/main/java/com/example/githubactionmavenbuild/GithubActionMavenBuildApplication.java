6f89c1aa73c89a9aaa3e8ac44e63959b944812cd
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-16 10:14:38 +0100

    Initialised Spring and updated git ignore

---
+++ src/main/java/com/example/githubactionmavenbuild/GithubActionMavenBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionmavenbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionMavenBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionMavenBuildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionmavenbuild/GithubActionMavenBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionmavenbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionMavenBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
