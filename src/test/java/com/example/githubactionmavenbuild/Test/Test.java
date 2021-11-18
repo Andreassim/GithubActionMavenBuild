2b3ab6494ac284ec4d5d99a13b914ac0c62d14d1
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-18 10:25:51 +0100

    new test

--- src/test/java/com/example/githubactionmavenbuild/Test/Test.java
+++ src/test/java/com/example/githubactionmavenbuild/Test/Test.java
@@ -8,4 +8,11 @@ public class Test {
     public void Test(){
         Assertions.assertEquals(2,1+1);
     }
+
+
+
+    @org.junit.jupiter.api.Test
+    public void Test2(){
+        Assertions.assertEquals(3,1+2);
+    }
 }
